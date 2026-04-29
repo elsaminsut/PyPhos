d05ea3fa12fbed4205f4d3fe9035f87b01ad43a8
Elsa Minsut <elsaminsut@gmail.com>
2026-04-29 11:44:36 +0200

    update: file structure refactor, add __init__.py

--- app/app.py
+++ app/app.py
@@ -1,6 +1,8 @@
 from contextlib import asynccontextmanager
+from database import SessionDep, lifespan
 from dotenv import load_dotenv
 from fastapi import Depends, FastAPI, HTTPException, Query
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from models import User, UserCreate, UserPublic, UserUpdate
 import os
 from sqlmodel import Session, SQLModel, create_engine, select
@@ -29,6 +31,27 @@ async def lifespan(app: FastAPI):
 SessionDep = Annotated[Session, Depends(get_session)]
 
 app = FastAPI(lifespan=lifespan)
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+def fake_decode_token(token):
+    return User(
+        username=token + "fakedecoded", email="john@example.com", full_name="John Doe"
+    )
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    user = fake_decode_token(token)
+    return user
+
+
+@app.get("/users/me")
+async def read_users_me(current_user: Annotated[User, Depends(get_current_user)]):
+    return current_user
+
+
+@app.get("/items/")
+async def read_items(token: Annotated[str, Depends(oauth2_scheme)]):
+    return {"token": token}
 
 @app.get("/")
 def index():
---
+++ app/utils/database.py
@@ -0,0 +1,29 @@
+from contextlib import asynccontextmanager
+from dotenv import load_dotenv
+from fastapi import Depends, FastAPI
+import os
+from sqlalchemy import create_engine
+from sqlmodel import Session, SQLModel
+from typing import Annotated
+
+load_dotenv()
+connection_string = os.getenv("DATABASE_URL")
+if not connection_string:
+    raise ValueError("DATABASE_URL is not set in your .env file")
+
+engine = create_engine(connection_string, echo=True)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    yield
+
+SessionDep = Annotated[Session, Depends(get_session)]
