fd4070d6d8083fd8fca7b12ab8f6483f4dc1750c
Elsa Minsut <elsaminsut@gmail.com>
2026-04-30 16:03:24 +0200

    update: refactor into routers/users, routers/auth

---
+++ routers/auth.py
@@ -0,0 +1,32 @@
+from app.models import User
+from app.utils.auth import (DUMMY_HASH, Token, create_access_token, verify_password)
+from app.utils.database import SessionDep
+from datetime import timedelta
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+from typing import Annotated
+from sqlmodel import select
+import os
+
+router = APIRouter()
+
+@router.post("/token")
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+    session: SessionDep) -> Token:
+
+    user = session.exec(select(User).where(User.username == form_data.username)).first()
+    password_to_check = user.hashed_password if user else DUMMY_HASH
+    password_valid = verify_password(form_data.password, password_to_check)
+
+    if not user or not password_valid:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=(int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))))
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return Token(access_token=access_token, token_type="bearer")
