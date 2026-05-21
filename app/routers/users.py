dc4cc36ba712720e1f364a6bfb39daf3becd4672
Elsa Minsut <elsaminsut@gmail.com>
2026-05-21 16:33:27 +0200

    fix: error handling in pv_calcs, remove redundant check in routers/users

--- app/routers/users.py
+++ app/routers/users.py
@@ -22,8 +22,6 @@ def read_users(session: SessionDep) -> list[User]:
 def update_user(current_user: CurrentUser, user: UserUpdate, session: SessionDep):
     """Update the current user's username and/or password. Updates the updated_at timestamp to the current time."""
     user_db = session.get(User, current_user.id)
-    if not user_db:
-        raise HTTPException(status_code=404, detail="User not found")
     user_data = user.model_dump(exclude_unset=True) # fields in UserUpdate, except those that are not set
     user_db.sqlmodel_update(user_data)
     session.add(user_db)
@@ -35,8 +33,6 @@ def update_user(current_user: CurrentUser, user: UserUpdate, session: SessionDep
 def delete_user(current_user: CurrentUser, session: SessionDep):
     """Delete the current user's account."""
     user_db = session.get(User, current_user.id)
-    if not user_db:
-        raise HTTPException(status_code=404, detail="User not found")
     session.delete(user_db)
     session.commit()
     return {"ok": True}
