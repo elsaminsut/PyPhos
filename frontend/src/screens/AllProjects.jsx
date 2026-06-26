bbf4ef29000d3b6186cbeb233ec9581c1849fae4
Elsa <elsaminsut@gmail.com>
2026-06-26 18:14:06 +0200

    wip: initial auth logic

--- frontend/src/screens/AllProjects.jsx
+++ frontend/src/screens/AllProjects.jsx
@@ -2,26 +2,39 @@ import Header from "../components/Header";
 import ProjectCard from "../components/ProjectCard";
 import './screens.css'
 import { Button } from "@/components/ui/button"
-import { useEffect, useState } from "react"
+import React, { useContext, useEffect, useState } from "react"
 
-export default function AllProjects() {
+const AllProjects = () => {
+    const { token } = useContext(AuthContext);
+    const [message, setMessage] = useState('');
     const [projects, setProjects] = useState([])
-
+    
     useEffect(() => {
-        fetch("/api/projects")
-        .then(res => res.json())
-        .then(data => setProjects(data))
-    }, [])
+        const fetchProtectedContent = async () => {
+            try {
+                const response = await fetch("/api/projects", {
+                    headers: { Authorization: `Bearer ${token}` }
+                });
+                
+                const data = await response.json();
+                setProjects(data);
+            } catch (error) {
+                setProjects('You are not allowed to view this content.');
+            }
+        };
+        
+        fetchProtectedContent();
+    }, [token])
 
     console.log(projects)
-
+    
     const projectList = projects.map(project => {
         <ProjectCard
             key={project.id}
             name={project.name} 
             location={project.location} />
     })
-
+    
     return (
     <>
         <Header />
@@ -40,3 +53,5 @@ export default function AllProjects() {
     </>
     )
 }
+
+export default AllProjects
