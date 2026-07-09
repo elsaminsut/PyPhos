afc997f8393ea3004b16c5a4e67a394c5ff5a009
Elsa Minsut <elsaminsut@gmail.com>
2026-07-09 19:58:37 +0200

    style: minor cosmetic fix (margin) and display system performance info as cards

--- frontend/src/screens/Scenario.jsx
+++ frontend/src/screens/Scenario.jsx
@@ -113,7 +113,7 @@ export default function Scenario() {
         <Header />
         <main className="max-w-[1000px] mx-auto px-8">
             <header className="flex-col mb-8">
-                <Breadcrumb>
+                <Breadcrumb className="my-4">
                     <BreadcrumbList>
                         <BreadcrumbItem>
                         <BreadcrumbLink href="/projects">Your projects</BreadcrumbLink>
