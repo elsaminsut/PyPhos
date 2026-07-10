7ad3bb32e2edc7a9023c1b77341e1fcd5ce9a026
Elsa Minsut <elsaminsut@gmail.com>
2026-07-10 15:48:37 +0200

    update: add favicon and logo in header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,11 @@
 import { Avatar, AvatarFallback } from "@/components/ui/avatar"
+import Logo from "../assets/pyphos-logo.svg"
 
 export default function Header() {
   return (
-    <header className="app-header">
-      <h1>PyPhos</h1>
+    <header className="flex justify-between items-center px-8 py-4 bg-background
+    sticky top-0 z-50 isolate">
+      <img src={Logo} alt="PyPhos Logo" className="h-8 w-auto"/>
       <Avatar>
         <AvatarFallback>E</AvatarFallback>
       </Avatar>
