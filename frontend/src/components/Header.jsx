1cc8ce26ef99aa582aa846240e6cef6c4c784580
Elsa Minsut <elsaminsut@gmail.com>
2026-07-14 15:57:57 +0200

    update: dropdown menu at avatar

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,4 +1,22 @@
-import { Avatar, AvatarFallback } from "@/components/ui/avatar"
+import {
+  SettingsIcon,
+  BellIcon,
+  CreditCardIcon,
+  LogOutIcon,
+} from "lucide-react"
+import {
+  Avatar,
+  AvatarFallback,
+} from "@/components/ui/avatar"
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 import Logo from "../assets/pyphos-logo.svg"
 
 export default function Header() {
@@ -6,9 +24,24 @@ export default function Header() {
     <header className="flex justify-between items-center px-8 py-4 bg-background
     sticky top-0 z-50 isolate">
       <img src={Logo} alt="PyPhos Logo" className="h-8 w-auto"/>
-      <Avatar>
-        <AvatarFallback>E</AvatarFallback>
-      </Avatar>
+      <DropdownMenu>
+      <DropdownMenuTrigger render={<Button variant="ghost" size="icon" className="rounded-full"><Avatar>
+          <AvatarFallback>E</AvatarFallback>
+        </Avatar></Button>} />
+      <DropdownMenuContent align="end">
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+            <SettingsIcon />
+            Settings
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <LogOutIcon />
+          Sign Out
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
     </header>
   )
 }
