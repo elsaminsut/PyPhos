b2cbd60dcf64455b7938cbe8e9c48066f1cba0d4
Elsa <elsaminsut@gmail.com>
2026-07-22 17:07:10 +0200

    fix: missing useEffect import

--- frontend/src/screens/Signup.jsx
+++ frontend/src/screens/Signup.jsx
@@ -1,4 +1,4 @@
-import { useState, useContext } from 'react'
+import { useState, useContext, useEffect } from 'react'
 import { useNavigate } from 'react-router'
 
 import { AuthContext } from '@/lib/AuthContext'
