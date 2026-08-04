0acfadf1b5b91955dd7d3398bd02deb6e23ad289
Elsa <elsaminsut@gmail.com>
2026-08-04 18:18:16 +0200

    update: add guest alert to header

---
+++ frontend/src/components/GuestAlert.jsx
@@ -0,0 +1,22 @@
+export default function GuestAlert() {
+    return (
+        <div className="page-container flex flex-col w-full my-4 z-100">
+            <div className=" bg-purple-soft text-white text-sm px-4 py-2 rounded-lg ">
+                <p className="text-center">You are using <span className="font-semibold">PyPhos</span> as a guest. To create multiple projects,{" "}
+                    <a
+                        href="/login"
+                        className="text-white hover:text-foreground hover:cursor-pointer hover:underline underline-offset-2"
+                    >
+                    log in
+                    </a> or {" "}
+                    <a
+                        href="/signup"
+                        className="text-white hover:text-foreground hover:cursor-pointer hover:underline underline-offset-2"
+                    >
+                    sign up.
+                    </a>
+                </p>
+            </div>
+        </div>
+    )
+}
